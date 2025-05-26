0220ddba92bf647828ce93b9d244b3120d41700b
SivaTejaArla <sivatejaarla@gmail.com>
2025-05-26 11:55:48 +0530

    CSRF disabled

--- src/main/java/com/example/SpringSecurity/controllers/GameController.java
+++ src/main/java/com/example/SpringSecurity/controllers/GameController.java
@@ -1,8 +1,14 @@
 package com.example.SpringSecurity.controllers;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
+
+    @GetMapping("")
+    public String Home(){
+        return  "Home ";
+    }
 }
